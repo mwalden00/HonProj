ca0ac87614d2efdba03406f51d1d7f44788997d9
Max Walden <s2098340@aonkenp920.inf.ed.ac.uk>
2024-03-21 23:33:51 +0000

    Fixed lorenz entropy file

--- scripts/lorenz_entropies.py
+++ scripts/lorenz_entropies.py
@@ -9,16 +9,15 @@ from copulagp.utils import Plot_Fit
 from copulagp import vine as v
 
 
-device = "cpu" if not torch.cuda.is_available() else "cuda"
+device = "cpu" if not torch.cuda.is_available() else "cuda:1"
 
 if __name__ == "__main__":
     with torch.device(device):
-        torch.set_default_device(device)
 
         with open("../models/results/lorenz_res.pkl", "rb") as f:
             lorenz_results = pkl.load(f)
 
-        with open("../models/results/random_lorenz_res.pkl", "rb") as f:
+        with open("../models/results/rand_lorenz_res.pkl", "rb") as f:
             rand_results = pkl.load(f)
 
         with open("../data/lorenz_vine_data_0.pkl", "rb") as f:
@@ -68,7 +67,7 @@ if __name__ == "__main__":
                     torch.arange(0, 1, 1.0 / 1500)
                 )
                 pupil_model_data[i][j] = cop
-        pupil_vine = v.CVine(pupil_model_data, torch.arange(0, 1, 1.0 / 1500))
+        pupil_vine = v.CVine(pupil_model_data, torch.arange(0, 1, 1.0 / 1500), device=device)
 
         random_model_data = copy.deepcopy(lorenz_results["models"])
 
@@ -76,8 +75,9 @@ if __name__ == "__main__":
             for j, cop_data in enumerate(layer):
                 cop = cop_data.model_init(device).marginalize(torch.rand(1500))
                 random_model_data[i][j] = cop
-        random_vine = v.CVine(random_model_data, torch.rand(1500))
-
+        random_vine = v.CVine(random_model_data, torch.rand(1500), device=device)
+        
+        print('Extracting entropies...')
         H = random_vine.entropy(v=True)
         print("Unparameterized Entropies:", H)
         H_X = pupil_vine.entropy(v=True)
