7dfb6dd188f4169c21b2ec3a74e985d87581cf5e
Max Walden <mxwaldo@Maxs-MacBook-Pro-50.local>
2024-03-21 23:45:32 +0000

    Neater code

--- scripts/extract_entropies.py
+++ scripts/extract_entropies.py
@@ -81,13 +81,21 @@ if __name__ == "__main__":
                 cop = cop_data.model_init(device).marginalize(torch.rand(1500))
                 random_model_data[i][j] = cop
         random_vine = v.CVine(random_model_data, torch.rand(1500), device=device)
+
+        print("Calculating entropies...")
+
         gc.collect()
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
-        print("Calculating entropies...")
+
         H = random_vine.entropy(v=True)
         np.savetxt("./rand_vine_entropies.csv", H.cpu().numpy(), delimiter=",")
         print("Unparameterized Entropies:", H)
+
+        gc.collect()
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
         H_X = pupil_vine.entropy(v=True)
         np.savetxt("./pupil_vine_entropies.csv", H_X.cpu().numpy(), delimiter=",")
         print("Parameterized Entropies:", H_X)
