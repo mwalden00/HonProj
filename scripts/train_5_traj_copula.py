c667d7586e1dd3177ced6c3083605962ef1b5ac1
Max Walden <mxwaldo@Maxs-MacBook-Pro-50.local>
2024-03-25 16:55:41 +0000

    Added entropy calc

--- scripts/train_5_traj_copula.py
+++ scripts/train_5_traj_copula.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle as pkl
 from copulagp.train import train_vine
+from copulagp import vine as v
+import copy
 
 # tell pandas to show all columns when we display a DataFrame
 pd.set_option("display.max_columns", None)
@@ -12,6 +14,7 @@ if torch.cuda.is_available():
     device_list = [f"cuda:{n}" for n in range(torch.cuda.device_count())]
 else:
     device_list = ["cpu"]
+device = device_list[0]
 
 if __name__ == "__main__":
     mp.set_start_method("spawn")
@@ -31,8 +34,9 @@ if __name__ == "__main__":
         np.concatenate([l[:100] for l in traj_and_pupil_data["pupil area"]])
     )
 
+    data = dict([("Y", Y.T), ("X", x)])
     with open("../data/pupil_vine_data_partial_0.pkl", "wb") as f:
-        pkl.dump(dict([("Y", Y.T), ("X", x)]), f)
+        pkl.dump(data, f)
 
     N = Y.shape[0]
 
@@ -46,3 +50,12 @@ if __name__ == "__main__":
         start=0,
         device_list=device_list,
     )
+
+    with open(f"../models/results/pupil_traj_{N}_res_partial.pkl", "rb") as f:
+        pupil_results = pkl.load(f)
+    pupil_model_data = copy.deepcopy(pupil_results["models"])
+    pupil_vine = v.CVine(
+        pupil_model_data, torch.Tensor(data["X"][:1500]), device=device
+    )
+    H = pupil_vine.entropy().cpu().detach().numpy()
+    print(f"Estimated entropy: {H} +/- {np.std(H)}")
