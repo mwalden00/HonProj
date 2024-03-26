37edec49dd1e3d0e8d8a2cb46eb73d1e0288becb
Max Walden <mxwaldo@Maxs-MacBook-Pro-50.local>
2024-03-26 19:45:12 +0000

    Quick fix

--- scripts/validate_bagging.py
+++ scripts/validate_bagging.py
@@ -167,17 +167,17 @@ if __name__ == "__main__":
             data = pkl.load(f)
 
         pupil_vine = get_random_vine(
-            5, torch.Tensor(data["X"][-5000:]), device=device, max_el=5
+            5, torch.Tensor(data["X"][-5000:]), device=device, max_el=10
         )
 
         print("True vine: ", pupil_vine.layers)
 
-        if args.skip_true_ent == 1:
+        """if args.skip_true_ent == 1:
             ent = np.genfromtxt("./true_ent.csv", delimiter=",")
         else:
             ent = pupil_vine.entropy().detach().cpu().numpy()
             ent.tofile("./true_ent.csv", sep=",")
-        print(f"Entropy extraction: {ent.mean()} +/- {2*np.std(ent)}")
+        print(f"Entropy extraction: {ent.mean()} +/- {2*np.std(ent)}")"""
 
         n_estimators = args.n_estimators
         assert 4000 % n_estimators == 0
